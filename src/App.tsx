aeb54e5b47beb8b81c75e8261d2fec4667785af7
lucasfrezzini <luquiitas.f@gmail.com>
2025-03-26 00:06:08 -0300

    Add Design System page and Home page in new public folder for better organization

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Home from "./pages/Home";
+import Home from "./pages/public/Home";
+import Login from "./pages/private/Login";
+import Register from "./pages/private/Register";
+import EditProfile from "./pages/private/EditProfile";
 
 function App() {
   return (
     <>
       <Navbar />
-      <Home></Home>
+      {/* <Home></Home> */}
+      {/* <Login></Login> */}
+      {/* <Register></Register> */}
+      {/* <Footer /> */}
+      <EditProfile></EditProfile>
       <Footer />
     </>
   );
---
+++ src/pages/public/Home/index.tsx
@@ -0,0 +1,44 @@
+import SectionHeader from "../../components/SectionHeader";
+import SectionHero from "../../components/SectionHero";
+import SectionLostPets from "../../components/SectionLostPets";
+import SectionTeam from "../../components/SectionTeam";
+import Button from "../../ui/Button";
+
+export default function Home() {
+  return (
+    <main>
+      <div className="bg-linear-to-b from-secondary to-transparent">
+        <div className="container md:pt-[60px] p-5 mx-auto">
+          <SectionHero
+            title="Encuentra a tus amigos peludos más rápido"
+            description="¿Tu mascota se perdió? ¡No estás solo! Reporta su pérdida y encuentra ayuda en nuestra comunidad. Descubre mascotas cerca y únete para ayudar a otros."
+          ></SectionHero>
+          <SectionLostPets></SectionLostPets>
+        </div>
+      </div>
+      <div className="container p-5 mx-auto">
+        <SectionTeam>
+          <SectionHeader
+            subtitle="Quienes somos"
+            textAlign="text-left"
+            title="Uniendo comunidades para salvar peluditos"
+          >
+            <p className="mb-4">
+              En Pet Rescue nos apasiona ayudar a las mascotas a regresar a
+              casa. Entendemos el dolor y la ansiedad que puede causar perder a
+              un ser querido, por eso creamos una plataforma que conecta a la
+              comunidad para facilitar el reencuentro.
+            </p>
+            <p className="mb-6">
+              Nuestra misión es ser una herramienta confiable y fácil de usar
+              para que las mascotas regresen a casa lo más rápido posible.
+              Queremos que nuestra DesignSystem sea un lugar donde la comunidad
+              se una para ayudar a aquellos que más lo necesitan.
+            </p>
+            <Button>Reportar mascota</Button>
+          </SectionHeader>
+        </SectionTeam>
+      </div>
+    </main>
+  );
+}
