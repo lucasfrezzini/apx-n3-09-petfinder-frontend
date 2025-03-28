7bb59dfcc991818a7af72092bd47b4bafbccc466
lucasfrezzini <luquiitas.f@gmail.com>
2025-03-28 04:06:24 -0300

    Add support for isStrech prop in Button component

--- src/ui/Button/index.tsx
+++ src/ui/Button/index.tsx
@@ -5,6 +5,7 @@ interface ButtonProps {
   children: ReactNode;
   isFull?: boolean;
   isUnfilled?: boolean;
+  isStrech?: boolean;
 }
 
 const defaultClasses =
@@ -13,16 +14,19 @@ const full = "w-full";
 const filled = "bg-primary text-white hover:bg-primary-dark";
 const unfilled =
   "bg-white border border-primary text-primary hover:bg-primary hover:text-white";
+const strech = "col-start-1 col-end-3";
 
 export default function Button({
   children,
   isUnfilled = false,
   isFull = false,
+  isStrech = false,
 }: ButtonProps) {
   const styles = clsx(
     defaultClasses,
     isUnfilled ? unfilled : filled,
-    isFull && full
+    isFull && full,
+    isStrech && strech
   );
   return <button className={styles}>{children}</button>;
 }
