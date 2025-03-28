9928c6d398eeae2ec4e37b08fb12cdf4e5c66f08
lucasfrezzini <luquiitas.f@gmail.com>
2025-03-28 20:01:58 -0300

    Refactor Badge component to simplify props and adjust styling

--- src/ui/Badge/index.tsx
+++ src/ui/Badge/index.tsx
@@ -3,19 +3,15 @@ import clsx from "clsx";
 
 interface BadgeProps {
   children: ReactNode;
-  isFound?: boolean;
   isLost?: boolean;
 }
 
-const defaultClasses = "px-2 py-2 rounded-(--border-radius) tracking-wide";
+const defaultClasses =
+  "px-1 py-1 rounded-(--border-radius) tracking-wide text-sm";
 const lost = "bg-alert-secondary text-alert-primary";
 const found = "bg-success-secondary text-success-primary";
 
-export default function Badge({
-  children,
-  isLost = false,
-  isFound = false,
-}: BadgeProps) {
-  const styles = clsx(defaultClasses, isLost && lost, isFound && found);
+export default function Badge({ children, isLost = true }: BadgeProps) {
+  const styles = clsx(defaultClasses, isLost ? lost : found);
   return <span className={styles}>{children}</span>;
 }
