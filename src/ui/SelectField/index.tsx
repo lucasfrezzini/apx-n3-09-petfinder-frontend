1fd1d646aa7c7e4a7473bb87e31d130b09e5ac2b
lucasfrezzini <luquiitas.f@gmail.com>
2025-04-05 02:07:40 -0300

    Add hook-form into SelectField

--- src/ui/SelectField/index.tsx
+++ src/ui/SelectField/index.tsx
@@ -2,17 +2,22 @@ import { ReactNode } from "react";
 
 interface SelectFieldProps {
   children: ReactNode;
-  name: string;
   autocomplete?: string;
+  register?: any;
 }
 
 const defaultClasses =
   "w-full p-3 border border-r-14 border-transparent border-(--color-gray-light) rounded-(--border-radius) outline outline-gray-light focus:outline-(--color-primary) transition-all";
 
 export default function SelectField(props: SelectFieldProps) {
-  const { children, name, autocomplete = "on" } = props;
+  const { children, register, autocomplete = "on" } = props;
   return (
-    <select name={name} autoComplete={autocomplete} className={defaultClasses}>
+    <select
+      {...register}
+      name={name}
+      autoComplete={autocomplete}
+      className={defaultClasses}
+    >
       {children}
     </select>
   );
