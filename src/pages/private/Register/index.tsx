e7540b4c52676630493f2d8f4d5cc091fec2a843
lucasfrezzini <luquiitas.f@gmail.com>
2025-04-05 02:06:05 -0300

    Add hook-form and schema validation on Register page

--- src/pages/private/Register/index.tsx
+++ src/pages/private/Register/index.tsx
@@ -1,36 +1,65 @@
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { RegisterSchema } from "../../../schemas/Register";
+
 import SectionPrivateLayout from "../../../components/SectionPrivateLayout";
 import Button from "../../../ui/Button";
 import FieldGroup from "../../../ui/FieldGroup";
 import InputField from "../../../ui/InputField";
+import InputFormError from "../../../ui/InputFormError";
 
 export default function Register() {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({
+    resolver: zodResolver(RegisterSchema),
+  });
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
+
   return (
     <main className="container mx-auto pt-24 sm:w-lg">
       <SectionPrivateLayout>
-        <h4>Crear cuenta nueva</h4>
-        <form className="my-10" action="">
+        <h4>Crear cuenta</h4>
+        <form className="my-10" onSubmit={handleSubmit(onSubmit)}>
           <FieldGroup label="Correo">
             <InputField
-              name="email"
+              register={register("email")}
               typeField="email"
               placeholder="Ingrese el correo"
             />
+            {errors.email?.message && (
+              <InputFormError>{errors.email?.message}</InputFormError>
+            )}
           </FieldGroup>
           <FieldGroup label="Tu contraseña">
             <InputField
-              name="password"
+              register={register("password")}
               typeField="password"
               placeholder="Ingrese la contrasena"
             />
+            {errors.password?.message && (
+              <InputFormError>{errors.password?.message}</InputFormError>
+            )}
           </FieldGroup>
           <FieldGroup label="Confirmar contraseña">
             <InputField
-              name="confirmPassword"
+              register={register("confirmPassword")}
               typeField="password"
               placeholder="Ingrese nuevamente la contrasena"
             />
+            {errors.confirmPassword?.message && (
+              <InputFormError>{errors.confirmPassword?.message}</InputFormError>
+            )}
           </FieldGroup>
-          <Button isFull>Registrarse</Button>
+          <Button isFull type="submit">
+            Registrarse
+          </Button>
         </form>
         <p className="text-gray-400">
           Ya tienes cuenta?{" "}
