1824280d74ec0104ffc732aafc6fe306fd6d8cb1
lucasfrezzini <luquiitas.f@gmail.com>
2025-04-05 02:05:52 -0300

    Add hook-form and schema validation on Login

--- src/pages/private/Login/index.tsx
+++ src/pages/private/Login/index.tsx
@@ -1,35 +1,62 @@
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { LoginSchema } from "../../../schemas/Login";
+
 import SectionPrivateLayout from "../../../components/SectionPrivateLayout";
 import Button from "../../../ui/Button";
 import FieldGroup from "../../../ui/FieldGroup";
 import InputField from "../../../ui/InputField";
+import { Link } from "react-router";
+import InputFormError from "../../../ui/InputFormError";
 
 export default function Login() {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({
+    resolver: zodResolver(LoginSchema),
+  });
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
+
   return (
     <main className="container mx-auto pt-24 sm:w-lg">
       <SectionPrivateLayout>
         <h4>Iniciar sesion</h4>
-        <form className="my-10" action="">
+        <form className="my-10" onSubmit={handleSubmit(onSubmit)}>
           <FieldGroup label="Correo">
             <InputField
-              name="email"
+              register={register("email")}
               typeField="email"
               placeholder="Ingrese el correo"
             />
+            {errors.email?.message && (
+              <InputFormError>{errors.email?.message}</InputFormError>
+            )}
           </FieldGroup>
           <FieldGroup label="Contrasena">
             <InputField
-              name="password"
+              register={register("password")}
               typeField="password"
               placeholder="Ingrese la contrasena"
             />
+            {errors.password?.message && (
+              <InputFormError>{errors.password?.message}</InputFormError>
+            )}
           </FieldGroup>
-          <Button isFull>Ingresar</Button>
+          <Button isFull type="submit">
+            Ingresar
+          </Button>
         </form>
         <p className="text-gray-400">
           No tienes cuenta?{" "}
-          <a className="text-primary hover:underline" href="#">
+          <Link className="text-primary hover:underline" to="/register">
             Crear una nueva cuenta
-          </a>
+          </Link>
         </p>
       </SectionPrivateLayout>
     </main>
