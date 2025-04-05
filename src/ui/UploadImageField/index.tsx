4b70402fd9121268eac2a6610d4bf51010950656
lucasfrezzini <luquiitas.f@gmail.com>
2025-04-05 02:09:01 -0300

    Update UploadImageField to configure with react-dropzone library

--- src/ui/UploadImageField/index.tsx
+++ src/ui/UploadImageField/index.tsx
@@ -1,9 +1,6 @@
 import picIcon from "../../assets/icons/pic.svg";
 
-interface UploadImageFieldProps {}
-
-export default function UploadImageField(props: UploadImageFieldProps) {
-  //! TODO: make get dataURI by images
+export default function UploadImageField() {
   return (
     <div className="mt-2 flex justify-center rounded-lg border border-dashed border-(--color-gray-light) px-6 py-10">
       <div className="text-center">
@@ -13,16 +10,10 @@ export default function UploadImageField(props: UploadImageFieldProps) {
             htmlFor="file-upload"
             className="relative cursor-pointer rounded-md bg-white font-semibold text-primary focus-within:ring-2 focus-within:ring-primary focus-within:ring-offset-2 focus-within:outline-hidden hover:text-primary-dark"
           >
-            <span>Upload a file</span>
-            <input
-              id="file-upload"
-              name="file-upload"
-              type="file"
-              className="sr-only"
-            />
+            <span>Subir Imagen</span>
           </label>
         </div>
-        <p className="text-xs/5 text-gray-400">PNG, JPG, GIF up to 10MB</p>
+        <p className="text-xs/5 text-gray-400">PNG, JPG hasta 5MB</p>
       </div>
     </div>
   );
