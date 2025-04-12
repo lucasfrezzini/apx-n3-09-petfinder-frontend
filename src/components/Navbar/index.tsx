9efa7636727f45be5695400eec6635bd91b16475
lucasfrezzini <luquiitas.f@gmail.com>
2025-04-12 02:30:37 -0300

    Add auth and options for login in the Navbar component. Manage routes when the user is unauthorized.

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -13,13 +13,28 @@ import { Link, NavLink } from "react-router";
 import close from "../../assets/icons/close.svg";
 import menu from "../../assets/icons/menu.svg";
 import petRescueLogo from "../../assets/petRescueLogo.png";
+import userDefault from "../../assets/userPhotoDefault.png";
 import Avatar from "../../ui/Avatar";
+import Button from "../../ui/Button";
+import { isAuthenticated } from "../../utils/auth";
+import { useLogout } from "../../hooks/login.hook";
+import { useAtom } from "jotai";
+import { userWithTokenAtom } from "../../context";
 
 const navigation = [
   { name: "Inicio", href: "/", current: false },
   { name: "Reporta tu mascota", href: "/create-pet-report", current: false },
   { name: "Mascotas perdidas", href: "/lost-pets", current: false },
-  { name: "Mapa", href: "lost-pets-map", current: false },
+  { name: "Mapa", href: "/lost-pets-map", current: false },
+];
+
+const navigationMobile = [
+  { name: "Inicio", href: "/", current: false },
+  { name: "Reporta tu mascota", href: "/create-pet-report", current: false },
+  { name: "Mascotas perdidas", href: "/lost-pets", current: false },
+  { name: "Mapa", href: "/lost-pets-map", current: false },
+  { name: "Iniciar sesión", href: "/login", current: false },
+  { name: "Regitrarse", href: "/register", current: false },
 ];
 
 function classNames(...classes: any) {
@@ -27,6 +42,9 @@ function classNames(...classes: any) {
 }
 
 export default function Navbar() {
+  const [user, _setUser] = useAtom(userWithTokenAtom);
+  const { logout } = useLogout();
+
   return (
     <Disclosure as="nav" className="bg-white w-full relative">
       <div className="z-50 fixed top-0 right-0 left-0 bg-white shadow-lg">
@@ -67,8 +85,8 @@ export default function Navbar() {
                     aria-current={item.current ? "page" : undefined}
                     className={({ isActive }: { isActive: boolean }) =>
                       isActive
-                        ? "text-primary rounded-md px-3 py-2 text-sm font-medium"
-                        : "rounded-md px-3 py-2 text-sm font-medium text-dark transition-colors hover:bg-primary hover:text-white"
+                        ? "text-primary rounded-md px-3 py-2"
+                        : "rounded-md px-3 py-2 text-dark transition-colors hover:bg-primary hover:text-white"
                     }
                   >
                     {item.name}
@@ -78,56 +96,72 @@ export default function Navbar() {
             </div>
           </div>
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-            {/* Profile dropdown */}
-            <Menu as="div" className="relative ml-3">
-              <div className="pr-3 sm:pr-0">
-                <MenuButton className="relative flex rounded-full bg-gray-800 text-sm focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden">
-                  <span className="absolute -inset-1.5" />
-                  <span className="sr-only">Open user menu</span>
-                  <Avatar
-                    src="https://avatars.githubusercontent.com/u/7221389?v=4"
-                    size="32px"
-                    isCircle
-                  />
-                </MenuButton>
+            {!isAuthenticated() ? (
+              <div className="hidden lg:flex lg:flex-1 lg:justify-end lg: gap-4">
+                <Link to={"/login"}>
+                  <Button type="button" isSmall isUnfilled>
+                    Iniciar sesión
+                  </Button>
+                </Link>
+                <Link to={"/register"}>
+                  <Button type="button" isSmall>
+                    Registrarse
+                  </Button>
+                </Link>
               </div>
-              <MenuItems
-                transition
-                className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 mr-3 sm:mr-0 ring-1 shadow-lg ring-black/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
-              >
-                <MenuItem>
-                  <Link
-                    to="/edit-profile"
-                    className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden"
-                  >
-                    Configuracion
-                  </Link>
-                </MenuItem>
-                <MenuItem>
-                  <Link
-                    to="/pets-state"
-                    className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden"
-                  >
-                    Mis mascotas
-                  </Link>
-                </MenuItem>
-                <MenuItem>
-                  <Link
-                    to="/logout"
-                    className="block px-4 py-2 text-sm border-t-1 border-gray-300 text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden"
-                  >
-                    Cerrar sesion
-                  </Link>
-                </MenuItem>
-              </MenuItems>
-            </Menu>
+            ) : (
+              <Menu as="div" className="relative ml-3">
+                <div className="pr-3 sm:pr-0">
+                  <MenuButton className="relative flex rounded-full text-sm cursor-pointer focus:outline-hidden">
+                    <span className="absolute -inset-1.5" />
+                    <span className="sr-only">Open user menu</span>
+                    <Avatar
+                      src={user?.profilePic ? user?.profilePic : userDefault}
+                      size="32px"
+                      isCircle
+                    />
+                  </MenuButton>
+                </div>
+                <MenuItems
+                  transition
+                  className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 mr-3 sm:mr-0 ring-1 shadow-lg ring-black/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
+                >
+                  <MenuItem>
+                    <Link
+                      to="/edit-profile"
+                      className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden"
+                    >
+                      Configuracion
+                    </Link>
+                  </MenuItem>
+                  <MenuItem>
+                    <Link
+                      to="/pets-state"
+                      className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden"
+                    >
+                      Mis mascotas
+                    </Link>
+                  </MenuItem>
+                  <MenuItem>
+                    <button
+                      onClick={() => {
+                        logout();
+                      }}
+                      className="block w-full px-4 py-2 cursor-pointer text-left text-sm border-t-1 border-gray-300 text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden"
+                    >
+                      Cerrar sesion
+                    </button>
+                  </MenuItem>
+                </MenuItems>
+              </Menu>
+            )}
           </div>
         </div>
       </div>
 
       <DisclosurePanel className="fixed z-40 top-[64px] bg-white w-full left-0 shadow-lg sm:hidden">
         <div className="space-y-1 px-2 pt-2 pb-3">
-          {navigation.map((item) => (
+          {navigationMobile.map((item) => (
             <Link to={item.href} key={item.name}>
               <DisclosureButton
                 key={item.name}
--- src/pages/private/PrivateLayout/index.tsx
+++ src/pages/private/PrivateLayout/index.tsx
@@ -1,12 +1,13 @@
-import { Outlet } from "react-router";
+import { Navigate, Outlet } from "react-router";
 import Navbar from "../../../components/Navbar";
+import { isAuthenticated } from "../../../utils/auth";
 
 export default function PrivateLayout() {
   return (
     <>
       <Navbar />
       <div className="bg-gray-light grid items-center justify-items-center min-h-svh">
-        {<Outlet />}
+        {isAuthenticated() ? <Outlet /> : <Navigate to="/login" />}
       </div>
     </>
   );
