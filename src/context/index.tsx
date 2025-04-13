4143cd8fd6305d83d2c445edde64c97f47dd76c2
lucasfrezzini <luquiitas.f@gmail.com>
2025-04-12 22:22:48 -0300

    Refactor context atoms to manage user pets and reports, and streamline user authentication data storage.

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,28 +1,27 @@
 import { atom } from "jotai";
 import { atomWithStorage } from "jotai/utils";
 
-import {
-  FetchParams,
-  Pet,
-  User,
-  Report,
-  UserWithToken,
-} from "../../src/utils/types";
+import { FetchParams, Pet, Report, UserWithToken } from "../../src/utils/types";
 
 // Atomo para la url base de la API
 export const urlBaseAtom = atom("http://localhost:3000/api");
 
-export const userAtom = atom<User | null>(null);
-
+// Atomo para guardar la data del usuario + el token de auth con localStorage
 export const userWithTokenAtom = atomWithStorage<UserWithToken | null>(
   "userWithToken",
   null
 );
 
+// Atomo para todas las mascotas
 export const petsAtom = atom<Pet[]>([]);
 
-export const reportsAtom = atom<Report[]>([]);
+// Atomo para las mascotas del usuario
+export const userPetsAtom = atom<Pet[]>([]);
 
+// Atomo para los reportes del usuario
+export const userReportsAtom = atom<Report[]>([]);
+
+// Atomo para la info de la mascota a reportar
 export const petAtom = atom<Pet | null>(null);
 
 // Atomo derivado para hacer el fetch dinamico para peticiones
