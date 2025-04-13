3a58310fe9afe79134eb414767c68cf7327a3ed7
lucasfrezzini <luquiitas.f@gmail.com>
2025-04-12 22:19:03 -0300

    Refactor PetResume component to use updated Pet type and add loading state handling in PetsState. Introduce PetResumeSkeleton for loading indication.

--- src/components/PetResume/index.tsx
+++ src/components/PetResume/index.tsx
@@ -1,28 +1,30 @@
 import Avatar from "../../ui/Avatar";
 import Badge from "../../ui/Badge";
 import Button from "../../ui/Button";
-
-type PetData = {
-  name: string;
-  isMissing: boolean;
-  urlImage: string;
-};
+import { Pet } from "../../utils/types";
 
 interface PetResumeProps {
-  pet: PetData;
+  pet: Pet;
+  isReport?: boolean;
 }
 
-export default function PetResume({ pet }: PetResumeProps) {
+export default function PetResume({ pet, isReport = false }: PetResumeProps) {
   return (
     <article className="flex gap-4 items-center">
-      <Avatar src={pet.urlImage} size={"64px"}></Avatar>
+      <Avatar src={pet.images[0].url} size={"64px"}></Avatar>
       <header className="grow">
         <p className="text-xl font-semibold mb-1">{pet.name}</p>
-        <Badge isLost={pet.isMissing}>
-          {pet.isMissing ? "Perdido" : "Ubicado"}
-        </Badge>
+        {isReport ? (
+          <p className="text-sm">Tienes 3 avistajes</p>
+        ) : (
+          <Badge isLost={pet.status == "lost" ? true : false}>
+            {pet.status == "lost" ? "Perdido" : "Ubicado"}
+          </Badge>
+        )}
       </header>
-      <Button isSmall>Editar</Button>
+      <Button type="button" isSmall>
+        {isReport ? "Ver" : "Editar"}
+      </Button>
     </article>
   );
 }
--- src/pages/private/PetsState/index.tsx
+++ src/pages/private/PetsState/index.tsx
@@ -2,63 +2,69 @@ import PetResume from "../../../components/PetResume";
 import SectionHeader from "../../../components/SectionHeader";
 import SectionPrivateLayout from "../../../components/SectionPrivateLayout";
 
-import emptyOK from "../../../assets/icons/emptyOK.svg";
-
-//! TODO use types of pets
-const mascotas = [
-  {
-    name: "Luna",
-    isMissing: true,
-    urlImage:
-      "https://www.cronista.com/files/image/651/651177/64bac94461355_700_462!.jpg?s=2e4395f6da95bbed148764512dc5558e&d=1689962824",
-  },
-  {
-    name: "Rocky",
-    isMissing: true,
-    urlImage:
-      "https://humanidades.com/wp-content/uploads/2017/02/perro-1-e1561678907722.jpg",
-  },
-  {
-    name: "Milo",
-    isMissing: false,
-    urlImage:
-      "https://cdn.shopify.com/s/files/1/0268/6861/files/cat-2908286_960_720_f3f025b4-912a-4e02-8701-0fdc8b3107db_grande.jpg?v=1533312369",
-  },
-  {
-    name: "Bella",
-    isMissing: true,
-    urlImage:
-      "https://www.hogarmania.com/archivos/201505/perro-consejos-848x477x80xX.jpg",
-  },
-];
+import { useGetUserPets } from "../../../hooks/user.hook";
+import { Pet } from "../../../utils/types";
+import PetResumeSkeleton from "../../../ui/PetResumeSkeleton";
 
 export default function PetsState() {
-  if (!mascotas.length) {
+  const { userPets, isLoading, error } = useGetUserPets();
+
+  if (error) {
     return (
-      <main className="container mx-auto pt-24 sm:w-lg">
-        <SectionHeader
-          title="Reportes creados"
-          subtitle="Estado de tus mascotas"
-        >
+      <main className="container mx-auto sm:w-lg">
+        <SectionHeader title="Mis Mascotas" subtitle="Estado de tus mascotas">
           <p className="mb-4">
-            No tienes mascotas reportadas ¡que buena noticia!
+            Tuvimos problemas tecnicos con la carga de la información
           </p>
         </SectionHeader>
         <div className="flex justify-center my-16">
-          <img src={emptyOK} alt="Empty Icon" />
+          <p className="text-8xl">😭</p>
         </div>
       </main>
     );
   }
+
+  if (isLoading) {
+    return (
+      <main className="container mx-auto sm:w-lg">
+        <SectionHeader title="Mis Mascotas" subtitle="Estado de tus mascotas">
+          <p className="mb-4">
+            No tienes mascotas reportadas aún ¡que buena noticia!
+          </p>
+        </SectionHeader>
+        <SectionPrivateLayout>
+          <div className="flex flex-col gap-6">
+            <PetResumeSkeleton bg="bg-secondary" />
+            <PetResumeSkeleton bg="bg-secondary" />
+            <PetResumeSkeleton bg="bg-secondary" />
+          </div>
+        </SectionPrivateLayout>
+      </main>
+    );
+  }
+
+  if (!userPets.length) {
+    return (
+      <main className="container mx-auto sm:w-lg">
+        <SectionHeader title="Mis Mascotas" subtitle="Estado de tus mascotas">
+          <p className="mb-4">Estas son tus mascotas reportadas en la app</p>
+        </SectionHeader>
+        <div className="flex justify-center my-16">
+          <p className="text-8xl">🥳</p>
+        </div>
+      </main>
+    );
+  }
+
   return (
-    <main className="container mx-auto pt-24 sm:w-lg">
-      <SectionHeader title="Reportes creados" subtitle="Estado de tus mascotas">
+    <main className="container mx-auto sm:w-lg">
+      <SectionHeader title="Mis Mascotas" subtitle="Estado de tus mascotas">
         <p className="mb-4">Estas son tus mascotas reportadas en la app</p>
       </SectionHeader>
       <SectionPrivateLayout>
         <div className="flex flex-col gap-6">
-          {mascotas.map((pet) => {
-            return <PetResume pet={pet} />;
+          {userPets.map((pet: Pet) => {
+            return <PetResume key={pet.name} pet={pet} />;
           })}
         </div>
       </SectionPrivateLayout>
