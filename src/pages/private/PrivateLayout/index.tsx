2d70f9e56ba977cc0d0c97d1584e6b2776e555b4
lucasfrezzini <luquiitas.f@gmail.com>
2025-04-21 04:59:52 -0300

    Update PrivateLayout to preserve location state on redirect to login

--- src/pages/private/PrivateLayout/index.tsx
+++ src/pages/private/PrivateLayout/index.tsx
@@ -1,13 +1,19 @@
-import { Navigate, Outlet } from "react-router";
+import { Navigate, Outlet, useLocation } from "react-router";
 import Navbar from "../../../components/Navbar";
 import { isAuthenticated } from "../../../utils/auth";
 
 export default function PrivateLayout() {
+  const location = useLocation();
+
   return (
     <>
       <Navbar />
       <div className="bg-gray-light grid items-center justify-items-center min-h-svh">
-        {isAuthenticated() ? <Outlet /> : <Navigate to="/login" />}
+        {isAuthenticated() ? (
+          <Outlet />
+        ) : (
+          <Navigate to="/login" replace state={{ from: location }} />
+        )}
       </div>
     </>
   );
