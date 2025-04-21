bce6986a3208144b435358cdbfe40092b97f968c
lucasfrezzini <luquiitas.f@gmail.com>
2025-04-21 04:48:39 -0300

    Update links and icons in the footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,37 +1,56 @@
+import { Link } from "react-router";
+
 export default function Footer() {
   return (
     <div className=" px-5 py-(--padding-section)">
       <nav aria-label="Footer" className="flex flex-wrap gap-6 justify-center">
-        <a href="#" className="text-dark transition-colors hover:text-primary">
+        <Link to="/" className="text-dark transition-colors hover:text-primary">
           Inicio
-        </a>
-        <a href="#" className="text-dark transition-colors hover:text-primary">
-          Nosotros
-        </a>
-        <a href="#" className="text-dark transition-colors hover:text-primary">
+        </Link>
+        <Link
+          to="/create-pet-report"
+          className="text-dark transition-colors hover:text-primary"
+        >
+          Reporta tú mascota
+        </Link>
+        <Link
+          to="/lost-pets"
+          className="text-dark transition-colors hover:text-primary"
+        >
           Mascotas Perdidas
-        </a>
-        <a href="#" className="text-dark transition-colors hover:text-primary">
-          Preguntas Frecuentes
-        </a>
+        </Link>
+        <Link
+          to="/lost-pets-map"
+          className="text-dark transition-colors hover:text-primary"
+        >
+          Mapa
+        </Link>
       </nav>
       <div className="flex flex-wrap mt-8 gap-6 justify-center">
-        <a href="#" className="size-6 transition-colors hover:text-primary">
-          <span className="hidden sr-only">Facebook</span>
+        <a
+          href="https://www.linkedin.com/in/lucasfrezzini/"
+          target="_blank"
+          className="size-6 transition-colors hover:text-primary"
+        >
+          <span className="hidden sr-only">Linkedin</span>
           <svg
-            fill="currentColor"
             viewBox="0 0 24 24"
+            fill="currentColor"
             aria-hidden="true"
             className="on"
           >
             <path
               fillRule="evenodd"
-              d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
+              d="M19.7065 3H4.34844C3.62264 3 3.04199 3.58065 3.04199 4.30645V19.6935C3.04199 20.3903 3.62264 21 4.34844 21H19.6485C20.3743 21 20.9549 20.4194 20.9549 19.6935V4.27742C21.013 3.58065 20.4323 3 19.7065 3ZM8.35491 18.3H5.71297V9.73548H8.35491V18.3ZM7.01942 8.54516C6.14846 8.54516 5.4807 7.84839 5.4807 7.00645C5.4807 6.16452 6.17749 5.46774 7.01942 5.46774C7.86136 5.46774 8.55813 6.16452 8.55813 7.00645C8.55813 7.84839 7.91942 8.54516 7.01942 8.54516ZM18.371 18.3H15.7291V14.1484C15.7291 13.1613 15.7001 11.8548 14.3356 11.8548C12.942 11.8548 12.7388 12.9581 12.7388 14.0613V18.3H10.0968V9.73548H12.6807V10.9258H12.7097C13.0872 10.229 13.9291 9.53226 15.2356 9.53226C17.9356 9.53226 18.4291 11.2742 18.4291 13.6548V18.3H18.371Z"
               clipRule="evenodd"
-            ></path>
+            />
           </svg>
         </a>
-        <a href="#" className="size-6 transition-colors hover:text-primary">
+        <a
+          href="https://www.instagram.com/lucasfrezzini/"
+          target="_blank"
+          className="size-6 transition-colors hover:text-primary"
+        >
           <span className="hidden sr-only">Instagram</span>
           <svg
             fill="currentColor"
@@ -46,18 +65,11 @@ export default function Footer() {
             ></path>
           </svg>
         </a>
-        <a href="#" className="size-6 transition-colors hover:text-primary">
-          <span className="hidden sr-only">X</span>
-          <svg
-            fill="currentColor"
-            viewBox="0 0 24 24"
-            aria-hidden="true"
-            className="on"
-          >
-            <path d="M13.6823 10.6218L20.2391 3H18.6854L12.9921 9.61788L8.44486 3H3.2002L10.0765 13.0074L3.2002 21H4.75404L10.7663 14.0113L15.5685 21H20.8131L13.6819 10.6218H13.6823ZM11.5541 13.0956L10.8574 12.0991L5.31391 4.16971H7.70053L12.1742 10.5689L12.8709 11.5655L18.6861 19.8835H16.2995L11.5541 13.096V13.0956Z"></path>
-          </svg>
-        </a>
-        <a href="#" className="size-6 transition-colors hover:text-primary">
+        <a
+          href="https://github.com/lucasfrezzini"
+          target="_blank"
+          className="size-6 transition-colors hover:text-primary"
+        >
           <span className="hidden sr-only">GitHub</span>
           <svg
             fill="currentColor"
@@ -68,21 +80,6 @@ export default function Footer() {
             <path
               fillRule="evenodd"
               d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
-              clipRule="evenodd"
-            ></path>
-          </svg>
-        </a>
-        <a href="#" className="size-6 transition-colors hover:text-primary">
-          <span className="hidden sr-only">YouTube</span>
-          <svg
-            fill="currentColor"
-            viewBox="0 0 24 24"
-            aria-hidden="true"
-            className="on"
-          >
-            <path
-              fillRule="evenodd"
-              d="M19.812 5.418c.861.23 1.538.907 1.768 1.768C21.998 8.746 22 12 22 12s0 3.255-.418 4.814a2.504 2.504 0 0 1-1.768 1.768c-1.56.419-7.814.419-7.814.419s-6.255 0-7.814-.419a2.505 2.505 0 0 1-1.768-1.768C2 15.255 2 12 2 12s0-3.255.417-4.814a2.507 2.507 0 0 1 1.768-1.768C5.744 5 11.998 5 11.998 5s6.255 0 7.814.418ZM15.194 12 10 15V9l5.194 3Z"
               clipRule="evenodd"
             ></path>
           </svg>
