213df480d0b74414f1aa7b9d12546c478b51f7d1
lucasfrezzini <luquiitas.f@gmail.com>
2025-04-21 04:50:19 -0300

    Update responsive and texts

--- src/components/PetResume/index.tsx
+++ src/components/PetResume/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router";
 import Avatar from "../../ui/Avatar";
 import Badge from "../../ui/Badge";
 import Button from "../../ui/Button";
@@ -5,26 +6,23 @@ import { Pet } from "../../utils/types";
 
 interface PetResumeProps {
   pet: Pet;
-  isReport?: boolean;
 }
 
-export default function PetResume({ pet, isReport = false }: PetResumeProps) {
+export default function PetResume({ pet }: PetResumeProps) {
   return (
     <article className="flex gap-4 items-center">
-      <Avatar src={pet.images[0].url} size={"64px"}></Avatar>
+      <Avatar src={pet.images[0].url} size={"48px"}></Avatar>
       <header className="grow">
-        <p className="text-xl font-semibold mb-1">{pet.name}</p>
-        {isReport ? (
-          <p className="text-sm">Tienes 3 avistajes</p>
-        ) : (
-          <Badge isLost={pet.status == "lost" ? true : false}>
-            {pet.status == "lost" ? "Perdido" : "Ubicado"}
-          </Badge>
-        )}
+        <p className="text-lg sm:text-xl font-semibold mb-1">{pet.name}</p>
+        <Badge isLost={pet.status == "lost" ? true : false}>
+          {pet.status == "lost" ? "Perdido" : "Ubicado"}
+        </Badge>
       </header>
-      <Button type="button" isSmall>
-        {isReport ? "Ver" : "Editar"}
-      </Button>
+      <Link to={`/edit-pet/${pet.id}`}>
+        <Button type="button" isSmall>
+          Editar mascota
+        </Button>
+      </Link>
     </article>
   );
 }
