8e15bf3cb8df601661b5031eef1872fb8ea2f510
lucasfrezzini <luquiitas.f@gmail.com>
2025-04-21 04:49:20 -0300

    Update icons, dinamic links and user profile pic in the Navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -14,27 +14,22 @@ import close from "../../assets/icons/close.svg";
 import menu from "../../assets/icons/menu.svg";
 import petRescueLogo from "../../assets/petRescueLogo.png";
 import userDefault from "../../assets/userPhotoDefault.png";
+import paws from "../../assets/icons/paw-print.png";
+import config from "../../assets/icons/cog.svg";
 import Avatar from "../../ui/Avatar";
 import Button from "../../ui/Button";
 import { isAuthenticated } from "../../utils/auth";
 import { useLogout } from "../../hooks/login.hook";
 import { useAtom } from "jotai";
 import { userWithTokenAtom } from "../../context";
+import Bell from "../../assets/icons/bell";
+import Exit from "../../assets/icons/exit";
 
 const navigation = [
   { name: "Inicio", href: "/", current: false },
-  { name: "Reporta tu mascota", href: "/create-pet-report", current: false },
+  { name: "Reporta tú mascota", href: "/create-pet-report", current: false },
   { name: "Mascotas perdidas", href: "/lost-pets", current: false },
   { name: "Mapa", href: "/lost-pets-map", current: false },
-];
-
-const navigationMobile = [
-  { name: "Inicio", href: "/", current: false },
-  { name: "Reporta tu mascota", href: "/create-pet-report", current: false },
-  { name: "Mascotas perdidas", href: "/lost-pets", current: false },
-  { name: "Mapa", href: "/lost-pets-map", current: false },
-  { name: "Iniciar sesión", href: "/login", current: false },
-  { name: "Regitrarse", href: "/register", current: false },
 ];
 
 function classNames(...classes: any) {
@@ -45,11 +40,19 @@ export default function Navbar() {
   const [user, _setUser] = useAtom(userWithTokenAtom);
   const { logout } = useLogout();
 
+  const navigationMobile = isAuthenticated()
+    ? navigation
+    : [
+        ...navigation,
+        { name: "Iniciar sesión", href: "/login", current: false },
+        { name: "Registrarse", href: "/register", current: false },
+      ];
+
   return (
     <Disclosure as="nav" className="bg-white w-full relative">
       <div className="z-50 fixed top-0 right-0 left-0 bg-white shadow-lg">
         <div className="container px-5 mx-auto relative flex h-16 items-center justify-between">
-          <div className="absolute inset-y-0 left-0 flex items-center pl-5 sm:pl-0 sm:hidden">
+          <div className="absolute inset-y-0 left-0 flex items-center pl-5 lg:pl-0 lg:hidden">
             {/* Mobile menu button*/}
             <DisclosureButton className="group relative inline-flex items-center justify-center cursor-pointer rounded-md focus:ring-2 focus:ring-white focus:outline-hidden focus:ring-inset">
               <span className="absolute -inset-0.5" />
@@ -66,7 +69,7 @@ export default function Navbar() {
               />
             </DisclosureButton>
           </div>
-          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+          <div className="flex flex-1 items-center justify-center lg:items-stretch lg:justify-start">
             <div className="flex shrink-0 items-center">
               <Link to="/">
                 <img
@@ -76,7 +79,7 @@ export default function Navbar() {
                 />
               </Link>
             </div>
-            <div className="hidden sm:ml-6 sm:block">
+            <div className="hidden lg:ml-6 lg:block">
               <div className="flex space-x-4">
                 {navigation.map((item) => (
                   <NavLink
@@ -116,7 +119,11 @@ export default function Navbar() {
                     <span className="absolute -inset-1.5" />
                     <span className="sr-only">Open user menu</span>
                     <Avatar
-                      src={user?.profilePic ? user?.profilePic : userDefault}
+                      src={
+                        user?.profilePic.url
+                          ? user?.profilePic.url
+                          : userDefault
+                      }
                       size="32px"
                       isCircle
                     />
@@ -124,29 +131,41 @@ export default function Navbar() {
                 </div>
                 <MenuItems
                   transition
-                  className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 mr-3 sm:mr-0 ring-1 shadow-lg ring-black/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
+                  className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white py-1 mr-3 sm:mr-0 ring-1 shadow-lg ring-black/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
                 >
                   <MenuItem>
                     <Link
                       to="/pets-state"
-                      className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden"
+                      className="flex gap-2 items-center px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden"
                     >
-                      Mis mascotas
+                      <img className="size-4" src={paws} />
+                      Mis mascotas reportadas
                     </Link>
                   </MenuItem>
                   <MenuItem>
                     <Link
                       to="/pets-reports"
-                      className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden"
+                      className="flex gap-2 items-center px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden"
                     >
-                      Mis avistajes
+                      <Bell />
+                      Alertas recibidas
                     </Link>
                   </MenuItem>
+                  {/* <MenuItem>
+                    <Link
+                      to="/user-reports"
+                      className="flex gap-2 items-center px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden"
+                    >
+                      <MapMarker />
+                      Avistamientos reportados
+                    </Link>
+                  </MenuItem> */}
                   <MenuItem>
                     <Link
                       to="/edit-profile"
-                      className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden"
+                      className="flex gap-2 items-center px-4 py-2 text-sm border-t-1 border-gray-300 text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden"
                     >
+                      <img className="size-4" src={config} />
                       Configuracion
                     </Link>
                   </MenuItem>
@@ -155,8 +174,9 @@ export default function Navbar() {
                       onClick={() => {
                         logout();
                       }}
-                      className="block w-full px-4 py-2 cursor-pointer text-left text-sm border-t-1 border-gray-300 text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden"
+                      className="flex gap-2 items-center w-full px-4 py-2 cursor-pointer border-t-1 border-gray-300 text-left text-sm  text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden"
                     >
+                      <Exit />
                       Cerrar sesion
                     </button>
                   </MenuItem>
@@ -167,25 +187,27 @@ export default function Navbar() {
         </div>
       </div>
 
-      <DisclosurePanel className="fixed z-40 top-[64px] bg-white w-full left-0 shadow-lg sm:hidden">
+      <DisclosurePanel className="fixed z-40 top-[64px] bg-white w-full left-0 shadow-lg lg:hidden">
         <div className="space-y-1 px-2 pt-2 pb-3">
-          {navigationMobile.map((item) => (
-            <Link to={item.href} key={item.name}>
-              <DisclosureButton
-                key={item.name}
-                as="button"
-                aria-current={item.current ? "page" : undefined}
-                className={classNames(
-                  item.current
-                    ? "text-primary"
-                    : "text-dark transition-colors hover:bg-primary hover:text-white",
-                  "block rounded-md px-3 py-2 text-base font-medium"
-                )}
-              >
-                {item.name}
-              </DisclosureButton>
-            </Link>
-          ))}
+          <div className="container mx-auto">
+            {navigationMobile.map((item) => (
+              <Link to={item.href} key={item.name}>
+                <DisclosureButton
+                  key={item.name}
+                  as="button"
+                  aria-current={item.current ? "page" : undefined}
+                  className={classNames(
+                    item.current
+                      ? "text-primary"
+                      : "text-dark transition-colors hover:bg-primary hover:text-white",
+                    "block rounded-md px-3 py-2 text-base font-medium"
+                  )}
+                >
+                  {item.name}
+                </DisclosureButton>
+              </Link>
+            ))}
+          </div>
         </div>
       </DisclosurePanel>
     </Disclosure>
