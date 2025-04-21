5bed0fd4e76247d14a076ce9991dc3620618a26d
lucasfrezzini <luquiitas.f@gmail.com>
2025-04-21 04:52:19 -0300

    Add new atoms for user coords, pet reports, updating types.

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,7 +1,13 @@
 import { atom } from "jotai";
-import { atomWithStorage } from "jotai/utils";
+import { atomWithStorage, createJSONStorage } from "jotai/utils";
 
-import { FetchParams, Pet, Report, UserWithToken } from "../../src/utils/types";
+import {
+  FetchParams,
+  Pet,
+  PetAndReports,
+  UserWithToken,
+  ReportInfo,
+} from "../../src/utils/types";
 
 // Atomo para la url base de la API
 export const urlBaseAtom = atom("http://localhost:3000/api");
@@ -12,14 +18,31 @@ export const userWithTokenAtom = atomWithStorage<UserWithToken | null>(
   null
 );
 
+//Creamos un storage con sessionStorage
+const storage = createJSONStorage<GeolocationCoordinates | null>(
+  () => sessionStorage
+);
+// Atomo para guardar las coords del user durante la session
+export const userCoordsAtom = atomWithStorage<GeolocationCoordinates | null>(
+  "userCoords",
+  null,
+  storage
+);
+
 // Atomo para todas las mascotas
 export const petsAtom = atom<Pet[]>([]);
 
 // Atomo para las mascotas del usuario
 export const userPetsAtom = atom<Pet[]>([]);
 
-// Atomo para los reportes del usuario
-export const userReportsAtom = atom<Report[]>([]);
+// // Atomo para la mascota y sus reportes
+// export const petWithReportsAtom = atom<PetAndReports[]>([]);
+
+// Atomo para el usuario con pets y reportes
+export const userPetsWithReportsAtom = atom<PetAndReports[]>([]);
+
+// Atomo para lista de reportes de una mascota
+export const petReportsAtom = atom<ReportInfo | null>(null);
 
 // Atomo para la info de la mascota a reportar
 export const petAtom = atom<Pet | null>(null);
