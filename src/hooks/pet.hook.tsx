b17bcaabf70bb3ce1e748a807aad43616c0bc1b5
lucasfrezzini <luquiitas.f@gmail.com>
2025-04-21 04:47:18 -0300

    Add hook to get pet by id, update pet report and change pet status. Update getPets for search all or near by user coords

--- src/hooks/pet.hook.tsx
+++ src/hooks/pet.hook.tsx
@@ -1,10 +1,16 @@
-import { useAtom, useSetAtom } from "jotai";
-import { asyncFetchDataAtom, petsAtom } from "../context";
+import { useAtom, useAtomValue, useSetAtom } from "jotai";
+import {
+  asyncFetchDataAtom,
+  petsAtom,
+  petAtom,
+  userCoordsAtom,
+} from "../context";
 import { useEffect, useState } from "react";
 import { hasToken } from "../utils/auth";
 import { Pet } from "../utils/types";
 
 export function useGetPets() {
+  const coords = useAtomValue(userCoordsAtom);
   const fetchData = useSetAtom(asyncFetchDataAtom);
   const [pets, setPets] = useAtom(petsAtom);
   const [isLoading, setIsLoading] = useState(true);
@@ -15,8 +21,12 @@ export function useGetPets() {
       try {
         setIsLoading(true);
 
+        const endpoint = coords
+          ? `pets/near?lat=${coords.latitude}&lng=${coords.longitude}`
+          : `pets`;
+
         const fetchPromise = fetchData({
-          endpoint: `pets`,
+          endpoint: endpoint,
           method: "GET",
           token: undefined,
           body: undefined,
@@ -24,6 +34,7 @@ export function useGetPets() {
         });
 
         const data = await fetchPromise;
+
         setPets(data);
       } catch (error: any) {
         setError(error);
@@ -32,7 +43,7 @@ export function useGetPets() {
       }
     }
     fetchApi();
-  }, [fetchData, setPets]);
+  }, [fetchData, setPets, coords]);
 
   return { pets, isLoading, error };
 }
@@ -58,4 +69,85 @@ export function useCreateNewReport() {
   }
 
   return { createReport };
+}
+
+export function useGetPetById(id: number) {
+  const fetchData = useSetAtom(asyncFetchDataAtom);
+  const [pet, setPet] = useAtom(petAtom);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    async function fetchApi() {
+      try {
+        setIsLoading(true);
+        const token = hasToken();
+        const endpoint = `pets/${id}`;
+        const fetchPromise = fetchData({
+          endpoint: endpoint,
+          method: "POST",
+          token: token as string,
+          body: undefined,
+          errorMessage: "Error gettin pet by if",
+        });
+
+        const data = await fetchPromise;
+
+        setPet(data);
+      } catch (error: any) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchApi();
+  }, [fetchData, setPet]);
+
+  return { pet, isLoading, error };
+}
+
+export function useUpdatePetReport() {
+  const fetchData = useSetAtom(asyncFetchDataAtom);
+
+  async function updatePetReport(data: Pet, id: number) {
+    try {
+      const token = hasToken();
+      const fetchPromise = fetchData({
+        endpoint: `pets/data`,
+        method: "PUT",
+        token: token as string,
+        body: { data, id },
+        errorMessage: "Error updating pet report",
+      });
+
+      await fetchPromise;
+    } catch (error: any) {
+      throw new Error(error);
+    }
+  }
+
+  return { updatePetReport };
+}
+
+export function useChangeStatusPet() {
+  const fetchData = useSetAtom(asyncFetchDataAtom);
+
+  async function changeStatusPet(id: number) {
+    try {
+      const token = hasToken();
+      const fetchPromise = fetchData({
+        endpoint: `pets/status`,
+        method: "PUT",
+        token: token as string,
+        body: { id },
+        errorMessage: "Error changing pet status",
+      });
+
+      await fetchPromise;
+    } catch (error: any) {
+      throw new Error(error);
+    }
+  }
+
+  return { changeStatusPet };
 }
