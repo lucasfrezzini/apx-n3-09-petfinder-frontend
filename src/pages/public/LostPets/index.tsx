a36b41a6848dbecfa236486b072cfd0e9e681aae
lucasfrezzini <luquiitas.f@gmail.com>
2025-04-21 04:53:25 -0300

    Integrate hook useGetPets in LostPets comp

--- src/pages/public/LostPets/index.tsx
+++ src/pages/public/LostPets/index.tsx
@@ -1,11 +1,18 @@
 import SectionLostPets from "../../../components/SectionLostPets";
+import { useGetPets } from "../../../hooks/pet.hook";
 
 export default function LostPets() {
+  const { pets, isLoading, error } = useGetPets();
+
   return (
     <main className="w-full">
       <div className="bg-linear-to-b from-secondary to-transparent">
         <div className="container md:pt-[60px] p-5 mx-auto">
-          <SectionLostPets></SectionLostPets>
+          <SectionLostPets
+            pets={pets}
+            isLoading={isLoading}
+            error={error}
+          ></SectionLostPets>
         </div>
       </div>
     </main>
