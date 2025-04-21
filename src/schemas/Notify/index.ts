c008c20982e546c5469f84fdd638c709bc9b86e3
lucasfrezzini <luquiitas.f@gmail.com>
2025-04-21 03:18:18 -0300

    Add required phone data in Notify

--- src/schemas/Notify/index.ts
+++ src/schemas/Notify/index.ts
@@ -3,8 +3,10 @@ import { z } from "zod";
 // creating a schema Notify
 export const NotifySchema = z.object({
   name: z.string().min(1, { message: "El nombre es obligatorio" }),
-  phone: z.string().optional(),
-  message: z.string().min(1, {
+  phone: z
+    .string()
+    .min(1, { message: "El teléfono es obligatorio para contactarte" }),
+  info: z.string().min(3, {
     message: "Por favor ingrese informacion adicional",
   }),
 });
